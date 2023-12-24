ab4faaed1aee86eafd9103120d3558e99b151ff8
monsieuremre <130907164+monsieuremre@users.noreply.github.com>
2023-12-24 14:38:15 +0000

    big fix

--- scripts/harden-fstab.sh
+++ scripts/harden-fstab.sh
@@ -8,112 +8,118 @@
 ## details. You should have received a copy of the GNU General Public License along with this program. If not, see
 ## <https://www.gnu.org/licenses/>.
 
-cp /etc/fstab /etc/fstab_old
-(grep -o '^[^#]*' /etc/fstab_old) >> /etc/fstab
-
-if [ ! -z "$(awk '$2=="/home"' /etc/fstab)" ]
+if [ -f /etc/hardened_tumbleweed/fstab_old ]
 then
-awk '$2=="/home" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/home" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
+    echo "Old configuration file is already backed up. Assuming hardening was already applied and skipping."
 else
-echo "/home /home none defaults,bind,nosuid,nodev 0 2" >> /etc/fstab
-fi
+	cp /etc/fstab /etc/hardened_tumbleweed/fstab_old
+	rm /etc/fstab
+	grep -o '^[^#]*' /etc/fstab_old | tee /etc/fstab
 
-if [ ! -z "$(awk '$2=="/tmp"' /etc/fstab)" ]
-then
-awk '$2=="/tmp" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/tmp" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/tmp" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "tmpfs /tmp tmpfs defaults,nodev,nosuid,noexec 0 0" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/home"' /etc/fstab)" ]
+	then
+		awk '$2=="/home" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/home" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/home /home none defaults,bind,nosuid,nodev 0 2" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/var/tmp"' /etc/fstab)" ]
-then
-awk '$2=="/var/tmp" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var/tmp" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var/tmp" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/tmp /var/tmp none defaults,bind,nosuid,nodev,noexec 0 0" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/tmp"' /etc/fstab)" ]
+	then
+		awk '$2=="/tmp" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/tmp" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/tmp" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "tmpfs /tmp tmpfs defaults,nodev,nosuid,noexec 0 0" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/var/log/audit"' /etc/fstab)" ]
-then
-awk '$2=="/var/log/audit" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var/log/audit" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var/log/audit" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/var/tmp"' /etc/fstab)" ]
+	then
+		awk '$2=="/var/tmp" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var/tmp" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var/tmp" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/var/tmp /var/tmp none defaults,bind,nosuid,nodev,noexec 0 0" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/var/log"' /etc/fstab)" ]
-then
-awk '$2=="/var/log" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var/log" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var/log" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/var/log /var/log none defaults,bind,nosuid,nodev,noexec 0 2" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/var/log/audit"' /etc/fstab)" ]
+	then
+		awk '$2=="/var/log/audit" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var/log/audit" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var/log/audit" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/var"' /etc/fstab)" ]
-then
-awk '$2=="/var" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/var" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/var /var none defaults,bind,nosuid,nodev 0 2" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/var/log"' /etc/fstab)" ]
+	then
+		awk '$2=="/var/log" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var/log" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var/log" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/var/log /var/log none defaults,bind,nosuid,nodev,noexec 0 2" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/dev/shm"' /etc/fstab)" ]
-then
-awk '$2=="/dev/shm" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/dev/shm" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/dev/shm" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "tmpfs /dev/shm tmpfs defaults,nosuid,nodev,noexec 0 0" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/var"' /etc/fstab)" ]
+	then
+		awk '$2=="/var" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/var" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/var /var none defaults,bind,nosuid,nodev 0 2" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/dev"' /etc/fstab)" ]
-then
-awk '$2=="/dev" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/dev" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "udev /dev devtmpfs defaults,nosuid,nodev,noexec 0 0" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/dev/shm"' /etc/fstab)" ]
+	then
+		awk '$2=="/dev/shm" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/dev/shm" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/dev/shm" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "tmpfs /dev/shm tmpfs defaults,nosuid,nodev,noexec 0 0" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/usr/share"' /etc/fstab)" ]
-then
-awk '$2=="/usr/share" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/usr/share" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/usr/share /usr/share none defaults,bind,nosuid,nodev 0 2" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/dev"' /etc/fstab)" ]
+	then
+		awk '$2=="/dev" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/dev" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "udev /dev devtmpfs defaults,nosuid,nodev,noexec 0 0" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/usr"' /etc/fstab)" ]
-then
-awk '$2=="/usr" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/usr /usr none defaults,bind,nodev 0 2" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/usr/share"' /etc/fstab)" ]
+	then
+		awk '$2=="/usr/share" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/usr/share" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/usr/share /usr/share none defaults,bind,nosuid,nodev 0 2" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/boot/efi"' /etc/fstab)" ]
-then
-awk '$2=="/boot/efi" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/boot/efi" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/boot/efi" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/usr"' /etc/fstab)" ]
+	then
+		awk '$2=="/usr" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/usr /usr none defaults,bind,nodev 0 2" >> /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/boot"' /etc/fstab)" ]
-then
-awk '$2=="/boot" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/boot" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/boot" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/boot /boot none defaults,bind,nodev,nosuid,noexec 0 2" >> /etc/fstab
-fi
+	if [ ! -z "$(awk '$2=="/boot/efi"' /etc/fstab)" ]
+	then
+		awk '$2=="/boot/efi" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/boot/efi" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/boot/efi" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	fi
 
-if [ ! -z "$(awk '$2=="/root"' /etc/fstab)" ]
-then
-awk '$2=="/root" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/root" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab >> /etc/fstab
-awk '$2=="/root" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab >> /etc/fstab
-else
-echo "/root /root none defaults,bind,nodev,nosuid,noexec 0 2" >> /etc/fstab
+	if [ ! -z "$(awk '$2=="/boot"' /etc/fstab)" ]
+	then
+	    awk '$2=="/boot" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/boot" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/boot" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/boot /boot none defaults,bind,nodev,nosuid,noexec 0 2" >> /etc/fstab
+	fi
+
+	if [ ! -z "$(awk '$2=="/root"' /etc/fstab)" ]
+	then
+		awk '$2=="/root" && $4!~"nosuid" {$4=$4",nosuid"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/root" && $4!~"nodev" {$4=$4",nodev"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+		awk '$2=="/root" && $4!~"noexec" {$4=$4",noexec"} 1' /etc/fstab > temp_fstab && mv -f temp_fstab /etc/fstab
+	else
+		echo "/root /root none defaults,bind,nodev,nosuid,noexec 0 2" >> /etc/fstab
+	fi
 fi
