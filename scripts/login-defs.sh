1ea0127cd81fddfafd02c3cc3df82da3c239dcbd
monsieuremre <130907164+monsieuremre@users.noreply.github.com>
2023-12-24 14:40:20 +0000

    login defs fix

--- scripts/login-defs.sh
+++ scripts/login-defs.sh
@@ -8,8 +8,12 @@
 ## details. You should have received a copy of the GNU General Public License along with this program. If not, see
 ## <https://www.gnu.org/licenses/>.
 
-cp /etc/login.defs /etc/login.defs_old
-echo "SHA_CRYPT_MIN_ROUNDS 10000
+if [ -f /etc/hardened_tumbleweed/login.defs_old ]
+then
+    echo "Old configuration file is already backed up. Assuming hardening was already applied and skipping."
+else
+	cp /etc/login.defs /etc/hardened_tumbleweed/login.defs_old
+	echo "SHA_CRYPT_MIN_ROUNDS 10000
 SHA_CRYPT_MAX_ROUNDS 100000" >> /etc/login.defs
-
-echo "HOME_MODE      0700" >> /etc/login.defs
+	echo "HOME_MODE      0700" >> /etc/login.defs
+fi
